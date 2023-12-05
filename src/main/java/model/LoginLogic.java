e9f3d84478990f42b08169865590a65498f4b3fa
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2023-12-05 16:56:47 +0900

    init

--- src/main/java/model/LoginLogic.java
+++ src/main/java/model/LoginLogic.java
@@ -1,7 +1,14 @@
 package model;
 
 import dao.BaseDao;
+import dao.UserDao;
 
 public class LoginLogic extends BaseDao {
+	public boolean  execute(User user) {
+		UserDao dao = new UserDao();
+		
+		boolean isLogin = dao.findByIdAndPassword(user);
+		return isLogin;
+	}
 
 }
