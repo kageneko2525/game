3f889a52a71fa2c2f474ea02d164565f6615df5d
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2023-12-05 17:01:41 +0900

    init

--- src/main/java/servlet/Sample.java
+++ src/main/java/servlet/Sample.java
@@ -1,5 +1,41 @@
 package servlet;
 
-public class Sample {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+//サーブレット名、クラス名はそれぞれ変更
+@WebServlet("/Test")
+public class Sample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Sample() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//リダイレクト先のパスを入れる変数
+		String path = null;
+		HttpSession session = request.getSession();
+
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    
+		//遷移先のサーブレットを指定
+		response.sendRedirect("Index");
+	}
 
 }
