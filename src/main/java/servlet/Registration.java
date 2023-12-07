3b5c22d6e1e3962dc01bfa05a1ccc740f67713cc
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2023-12-07 17:19:10 +0900

    ログイン処理行使ん

--- src/main/java/servlet/Registration.java
+++ src/main/java/servlet/Registration.java
@@ -2,40 +2,40 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class Registration
- */
+
 @WebServlet("/Registration")
 public class Registration extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Registration() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//リダイレクト先のパスを入れる変数
+		String path = null;
+		HttpSession session = request.getSession();
+
+		path = "/WEB-INF/jsp/registration.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+		dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	    
+
+	    
+		//遷移先のサーブレットを指定
+		response.sendRedirect("Index");
 	}
 
 }
