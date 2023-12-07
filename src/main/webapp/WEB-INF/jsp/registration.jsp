e4d0f278a8a19b86686e12fe310ab24a4699a746
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2023-12-07 17:15:34 +0900

    登録処理

--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -8,10 +8,16 @@
 </head>
 <body>
 	<form action="Registration" method="post">
-	ユーザー名 <input type="text" class="form__inputText" name="userName"><br>
-	パスワード<input type="password" class="form__inputText" name="pass1">
-	パスワード再入力<input type="password"class="form__inputText" name="pass2">
-	<input type="submit" value="送信">
+
+		ユーザー名 <input type="text" id="form__inputText1" name="userName"><br>
+		<div id="error1" style="color: red;"></div>
+		パスワード<input type="password" id="form__inputText2" name="pass1"><br>
+		<div id="error2" style="color: red;"></div>
+		パスワード再入力<input type="password" id="form__inputText3" name="pass2">
+		<div id="error3" style="color: red;"></div>
+		<input id="form--button" type="submit" value="送信">
+
+	</form>
 	</form>
 </body>
 </html>
--- src/main/webapp/js/registration.js
+++ src/main/webapp/js/registration.js
@@ -1,34 +1,98 @@
-/**
- * 登録処理
- * 製作者ねこ
- */
+		function changeButton() {
+			let button = document.getElementById("form--button");
+			button.type = "button"
+			button.value = "送信できません"
+		}
 
-        document.addEventListener("DOMContentLoaded", function () {
-            // テキスト入力時に呼び出される関数
-            function checkInput(event) {
-                // イベントが発生した要素からクラス名が "inputClass" のものを取得
-                if (event.target.classList.contains("inputClass")) {
-                    // ユーザーが入力したテキストを取得
-                    var userInput = event.target.value;
+		function changeSubmit() {
+			let button = document.getElementById("form--button");
+			button.type = "submit"
+			button.value = "送信"
+		}
 
-                    // 文字数を取得
-                    var charCount = userInput.length;
+		document.addEventListener("DOMContentLoaded", function () {
+			// テキスト入力時に呼び出される関数
+			function checkInputUserName() {
+				// ユーザーが入力したテキストを取得
+				var userInput = document.getElementById("form__inputText1").value;
+				if (userInput != "") {
+					// 正規表現を使用して検証
+					var pattern = /^[a-zA-Z0-9_]+$/;
 
-                    // 例: 文字数が制限を超えている場合にエラーメッセージを表示
-                    var maxCharCount = 10; // 最大文字数
-                    if (charCount > maxCharCount) {
-                        document.getElementById("error").innerText = "文字数は" + maxCharCount + "文字以下にしてください";
-                    } else {
-                        document.getElementById("error").innerText = "";
-                    }
+					// 例: 正規表現に一致しない場合にエラーメッセージを表示
+					if (!pattern.test(userInput)) {
+						document.getElementById("error1").innerText = "大文字、小文字の英字、数字、アンダースコアのみ使用できます ";
+						changeButton()
+					} else if (userInput.length > 16) {
+						document.getElementById("error1").innerText = "ユーザー名は16文字までです";
+						changeButton()
+					} else {
+						document.getElementById("error1").innerText = "";
+						changeSubmit()
+					}
+				} else {
+					document.getElementById("error1").innerText = "";
+					changeSubmit()
+				}
+				// 他の検証ルールを追加することができます
+			}
+			// input要素に対してinputイベントを追加
+			document.getElementById("form__inputText1").addEventListener("input", checkInputUserName);
+		});
 
-                    // 他の検証ルールを追加することができます
-                }
-            }
 
-            // クラス名が "form__inputText" の全ての input 要素に対してkeyupイベントを追加
-            var inputElements = document.querySelectorAll(".form__inputText");
-            inputElements.forEach(function (inputElement) {
-                inputElement.addEventListener("keyup", checkInput);
-            });
-        });
+		document.addEventListener("DOMContentLoaded", function () {
+			// テキスト入力時に呼び出される関数
+			function checkInputPass() {
+				// ユーザーが入力したテキストを取得
+				var userInput = document.getElementById("form__inputText2").value;
+				if (userInput != "") {
+					// 正規表現を使用して検証
+					var pattern = /^[a-zA-Z~`!@#$%^&*()-_+=]+$/;
+
+					// 例: 正規表現に一致しない場合にエラーメッセージを表示
+					if (!pattern.test(userInput)) {
+						document.getElementById("error2").innerText = "使用できない文字が含まれています";
+						changeButton()
+					} else {
+						document.getElementById("error1").innerText = "";
+						changeSubmit()
+					}
+
+				} else {
+					document.getElementById("error2").innerText = "";
+					changeSubmit()
+				}
+				// 他の検証ルールを追加することができます
+			}
+			// input要素に対してinputイベントを追加
+			document.getElementById("form__inputText2").addEventListener("input", checkInputPass);
+		});
+
+
+		document.addEventListener("DOMContentLoaded", function () {
+			// テキスト入力時に呼び出される関数
+			function checkInputSamePass() {
+				// ユーザーが入力したテキストを取得
+				var userInput1 = document.getElementById("form__inputText2").value;
+				var userInput2 = document.getElementById("form__inputText3").value;
+				if (userInput2 != "") {
+
+					// パスワードが一致しないときエラー表示
+					if (userInput1 != userInput2) {
+						document.getElementById("error3").innerText = "パスワードが一致しません";
+						changeButton()
+					} else {
+						document.getElementById("error3").innerText = "";
+						changeSubmit()
+					}
+
+				} else {
+					document.getElementById("error3").innerText = "";
+					changeSubmit()
+				}
+				// 他の検証ルールを追加することができます
+			}
+			// input要素に対してinputイベントを追加
+			document.getElementById("form__inputText3").addEventListener("input", checkInputSamePass);
+		});
