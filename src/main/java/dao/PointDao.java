016193d20d09e8f983941a920a847420348ba2a0
esegamesshu <152826080+esegamesshu@users.noreply.github.com>
2023-12-11 17:39:49 +0900

    point関連

--- src/main/java/dao/PointDao.java
+++ src/main/java/dao/PointDao.java
@@ -1,5 +1,45 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+import model.Point;
+
 public class PointDao extends BaseDao {
 
+	public point getPoint() {
+		return null;
+	}
+
+	public int UpdatePoint(Point point ) {
+		try {
+			this.connect();
+			
+			String sql = "UPDATE point set point=point+ where user_id=?";
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			
+			
+			
+			//PointLogicでゲームで獲得したポイントを入れる変数作ってここにセット
+			ps.setInt(1, );
+			ps.setInt(2, point.getUserId());
+			
+			//PointLogicでゲーム結果とショップどちらで減るのか分かる変数作ってif文の条件に入れる(多分色々変わる)
+			if() {
+			point.setSumPoint();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				this.disConnect();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return point.getPoint();
+	}
 }
---
+++ src/main/java/model/Point.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Point {
+	private int userId;
+	private int point;
+	private int sumPoint;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public int getSumPoint() {
+		return sumPoint;
+	}
+
+	public void setSumPoint(int sumPoint) {
+		this.sumPoint = sumPoint;
+	}
+
+}
