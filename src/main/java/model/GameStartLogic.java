41238683cacadc5b4c22f5d86cc7009b1f5231bf
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2024-01-12 12:44:18 +0900

    ゲームスタート関連

--- src/main/java/model/GameStartLogic.java
+++ src/main/java/model/GameStartLogic.java
@@ -1,5 +1,48 @@
 package model;
 
+import dao.PointDao;
+import dao.UsePointDao;
+
 public class GameStartLogic {
 
+	/**
+	 * ゲームと難易度が存在するかチェック
+	 * @param gameId ゲームID
+	 * @param levelId 難易度ID
+	 * @return true:存在する false:存在しない
+	 */
+	public boolean checkGameLevel(int gameId, int levelId) {
+		UsePointDao usePointDao = new UsePointDao();
+		if (usePointDao.getUsePoint(gameId, levelId) > 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	/**
+	 * 必要なポイントに十分なポイントを所持しているか
+	 * @param user ログインユーザー
+	 * @param gameId ゲームID
+	 * @param levelId 難易度ID
+	 * @param point ユーザーのポイント格納用
+	 * @return true：十分なポイントがある false：十分なポイントがない
+	 */
+	public Boolean checkEnoughPoint(User user, int gameId, int levelId, Point point) {
+		//pointテーブル検索用
+		PointDao pointDao = new PointDao();
+		UsePointDao usePointDao = new UsePointDao();
+
+		if (pointDao.getPoint(point)) {
+
+		} else {
+			//エラーだねえ
+
+			return false;
+		}
+
+		return point.getPoint() - usePointDao.getUsePoint(gameId, levelId) >= 0;
+
+	}
 }
--- src/main/java/servlet/GameStart.java
+++ src/main/java/servlet/GameStart.java
@@ -10,39 +10,65 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.GameStartLogic;
+import model.Point;
+import model.User;
 
 @WebServlet("/GameStart")
 public class GameStart extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-    public GameStart() {
-        super();
+	public GameStart() {
+		super();
 
-    }
+	}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//リダイレクト先のパスを入れる変数
-		String path = null;
-		HttpSession session = request.getSession();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-
-		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-		dispatcher.forward(request, response);
-		
 		response.sendRedirect("Index");
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		//リダイレクト先のパスを入れる変数
 		String path = null;
 		HttpSession session = request.getSession();
 
+		if (session == null) {
+			path = "/WEB-INF/jsp/login.jsp";
+		}
 
+		User user = (User) session.getAttribute("loginUser");
+
+		int gameId = -1;
+		int levelId = -1;
+		
+		Point point = new Point();
+		
+		try {
+
+			gameId = Integer.parseInt(request.getParameter("gameId"));
+			levelId = Integer.parseInt(request.getParameter("levelId"));
+
+		} catch (Exception e) {
+
+			//存在しない難易度の場合ゲームメニューへ飛ばす
+			path = "/WEB-INF/jsp/gamemenu.jsp";
+		}
+
+		GameStartLogic gameStartLogic = new GameStartLogic();
+
+		if (!gameStartLogic.checkGameLevel(gameId, levelId)) {
+			//存在しない難易度の場合ゲームメニューへ飛ばす
+			path = "/WEB-INF/jsp/gamemenu.jsp";
+		}
+
+		gameStartLogic.checkEnoughPoint(user, gameId, levelId, point);
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
 		dispatcher.forward(request, response);
 	}
-
 
 }
