048065147ae0f659d3a48edc86ab0348bbd1bda3
esegamesshu <152826080+esegamesshu@users.noreply.github.com>
2024-01-15 15:37:47 +0900

    質問書いた

--- src/main/java/servlet/GameStart.java
+++ src/main/java/servlet/GameStart.java
@@ -14,6 +14,12 @@ import model.GameStartLogic;
 import model.Point;
 import model.User;
 
+
+
+//初期化どこ
+
+
+
 @WebServlet("/GameStart")
 public class GameStart extends HttpServlet {
 	private static final long serialVersionUID = 1L;
