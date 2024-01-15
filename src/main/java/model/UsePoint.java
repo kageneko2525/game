55e3ed0e483d4b1a075538f46ebe48d110656531
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2024-01-15 13:40:45 +0900

    DTO作成

---
+++ src/main/java/model/UsePoint.java
@@ -0,0 +1,31 @@
+package model;
+
+public class UsePoint {
+	private int gameId ;
+	private int levelId;
+	private int usePount;
+	
+	
+	
+	public int getGameId() {
+		return gameId;
+	}
+	public void setGameId(int gameId) {
+		this.gameId = gameId;
+	}
+	public int getLevelId() {
+		return levelId;
+	}
+	public void setLevelId(int levelId) {
+		this.levelId = levelId;
+	}
+	public int getUsePount() {
+		return usePount;
+	}
+	public void setUsePount(int usePount) {
+		this.usePount = usePount;
+	}
+	
+	
+	
+}
