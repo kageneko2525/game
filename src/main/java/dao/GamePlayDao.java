9a6b4d35884312fcd739280ce6cd02e9cdce798e
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2024-01-16 10:18:47 +0900

    ゲームスタート処理

--- src/main/java/dao/GamePlayDao.java
+++ src/main/java/dao/GamePlayDao.java
@@ -1,5 +1,56 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.GamePlay;
+
 public class GamePlayDao extends BaseDao {
 
+	
+	/**
+	 * ゲームプレイ表のインサート
+	 * スタートは1　endは0に設定しとくよ
+	 * @param gamePlay playid,gameidはセットしといてね
+	 * @return true:成功 false:失敗
+	 */
+	public boolean gamePlaySetup(GamePlay gamePlay) {
+		try {
+
+			this.connect();
+			
+			String sql = "INSERT INTO game_play (play_id , game_id , start, end ) VALUES (?, ?, '1', '0')";
+
+			PreparedStatement ps = con.prepareStatement(sql);
+
+
+			ps.setString(1, Integer.toString(gamePlay.getPlayId()));
+			ps.setString(2, Integer.toString(gamePlay.getGameId()));
+
+			gamePlay.setStart(1);
+			gamePlay.setEnd(0);
+			
+			
+			ps.executeUpdate();
+
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				this.disConnect();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+
+	}
+	
+	
+	
+	
+	
+	
+	
 }
